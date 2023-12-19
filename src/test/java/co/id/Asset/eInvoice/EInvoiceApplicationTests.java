e87d60c32734c5bd5f1d8639f7ef3bd4e365ec83
Daffa Rizki <daffa.prasetia@indocyber.id>
2023-12-19 11:09:11 +0700

    test case

--- src/test/java/co/id/Asset/eInvoice/EInvoiceApplicationTests.java
+++ src/test/java/co/id/Asset/eInvoice/EInvoiceApplicationTests.java
@@ -1,13 +1,21 @@
 package co.id.Asset.eInvoice;
 
+import co.id.Asset.eInvoice.Service.InvoiceService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EInvoiceApplicationTests {
 
+	@Autowired
+	private InvoiceService invoiceService;
+
 	@Test
-	void contextLoads() {
+	void testGenerator() {
+		String code = "A01";
+
+		invoiceService.invoiceNumberGenerator(code);
 	}
 
 }
